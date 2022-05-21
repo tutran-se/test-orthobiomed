308d7399a244a4dc48a7703d89e125a35d4882ba
Tu Tran <trandinhtu.dev@gmail.com>
2022-05-20 22:04:07 -0400

    Fix sameSite cookie

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,11 +2,12 @@
 
 import Cookies from "cookies";
 export default function handler(req, res) {
-  console.log(req.query);
+  console.log(process.env.NODE_ENV);
 
   const cookies = new Cookies(req, res);
   cookies.set("tokens", JSON.stringify(req.query), {
-    httpOnly: false, // true by default
+    httpOnly: process.env.NODE_ENV === "production" ? true : false, // true by default
+    sameSite: "strict",
   });
 
   res.redirect(307, "/");
